4e8ece322ada6b95a9fe607f4dd79def1002bb46
Stanlay HOUNNOUVI <stanlayhounnouvi@gmail.com>
2023-04-24 18:41:52 +0100

    Initial project paths

---
+++ src/test/java/com/services/expresso/ExpressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.expresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
